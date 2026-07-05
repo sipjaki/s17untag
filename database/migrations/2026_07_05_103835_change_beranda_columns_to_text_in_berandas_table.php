678214208d2cfdf436675e96e7e4dfac1b9b3bbe
SipjakiBlora <sipjakiblora@gmail.com>
2026-07-05 17:39:18 +0700

    rubahan yang baru

---
+++ database/migrations/2026_07_05_103835_change_beranda_columns_to_text_in_berandas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('berandas', function (Blueprint $table) {
+            $table->text('sabha1')->nullable()->change();
+            $table->text('sabha2')->nullable()->change();
+            $table->text('sabha3')->nullable()->change();
+            $table->text('sabha4')->nullable()->change();
+            $table->text('sabha5')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('berandas', function (Blueprint $table) {
+            $table->string('sabha1')->nullable()->change();
+            $table->string('sabha2')->nullable()->change();
+            $table->string('sabha3')->nullable()->change();
+            $table->string('sabha4')->nullable()->change();
+            $table->string('sabha5')->nullable()->change();
+        });
+    }
+};
